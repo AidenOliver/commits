73516f4f46e19b7d2647a29fc50f913c929c7ba6
Aiden Oliver <31047390+AidenOliver@users.noreply.github.com>
2020-04-22 12:11:25 -0600

    added fetch function to useEffect and finished JSX

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+  const [page, setPage] = useState(1);
+  const [commits, setCommits] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const loadMoreCommits = () => {
+    setPage(page + 1);
+  }
+
+  useEffect(() => {
+    fetch(`https://api.github.com/search/commits?q=repo:facebook/react+css&page=${page}`,
+    {
+      method: "GET",
+      headers: new Headers({
+        Accept: "application/vnd.github.cloak-preview"
+      })
+    }
+    )
+    .then(res => res.json())
+    .then(response => {
+      setCommits(response.items);
+      setLoading(false);
+    })
+    .catch(error => console.log(error));
+  }, [page]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Commits</h1>
+      {loading && <p>Loading commits</p>}
+
+      {commits.length !== 0 && (
+        <button onClick={loadMoreCommits}>Load More Commits</button>
+      )}
+
+      {commits.map((c, index) => (
+        <div key={index}>
+          {c.commit && (
+            <>
+              <div>
+                <h2>
+                  {c.commit.committer.name}
+                </h2>
+                <p>
+                  {c.commit.message}
+                </p>
+              </div>
+            </>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
